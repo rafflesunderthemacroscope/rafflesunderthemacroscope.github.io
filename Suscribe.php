ea2cb77d4af92d383458d12d3b65c9da93d278a1
ChinJY <chinjy2@gmail.com>
2016-10-07 12:53:27 +0800

    update

---
+++ Suscribe.php
@@ -0,0 +1,10 @@
+<?php
+if(lisset ($_POST['submit']))
+{
+	echo "error; you need to submit the form!"
+}
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$message  = $_POST['message'];
+
+if(empty($))
