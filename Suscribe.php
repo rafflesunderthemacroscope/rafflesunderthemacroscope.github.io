c08b3634dbe73c9f504805cce8b6b4801cb9921e
ChinJY <chinjy2@gmail.com>
2016-10-07 21:47:55 +0800

    update

--- Suscribe.php
+++ Suscribe.php
@@ -7,4 +7,13 @@ $name = $_POST['name'];
 $visitor_email = $_POST['email'];
 $message  = $_POST['message'];
 
-if(empty($))
+if(empty($name)||empty($visitor_email))
+{
+	echo "Name and email are mandatory!";
+	exit;
+}
+
+$email_from = 'underthemacroscope@gmail.com';
+$email_subject = "New email";
+$email_body = "You have received a new message from $name";
+$to
