844c37028394f741880e03f36b32ba47efe5d2ca
C <the>
2016-10-07 22:12:04 +0800

    update

--- Suscribe.php
+++ Suscribe.php
@@ -1,19 +1,23 @@
-<?php
+ <?php
 if(lisset ($_POST['submit']))
 {
 	echo "error; you need to submit the form!"
 }
-$name = $_POST['name'];
-$visitor_email = $_POST['email'];
-$message  = $_POST['message'];
 
-if(empty($name)||empty($visitor_email))
+$visitor_email = $_POST['email'];
+
+if(empty($visitor_email))
 {
 	echo "Name and email are mandatory!";
 	exit;
 }
 
-$email_from = 'underthemacroscope@gmail.com';
+$email_from = "underthemacroscope@gmail.com";
 $email_subject = "New email";
-$email_body = "You have received a new message from $name";
-$to
+$email_body = "New suscriber.\n".
+			"email address: $visitor_email\n".
+
+$to = "underthemacroscope@gmail.com";
+$headers = "From: $email_from \r\n";
+
+mail($to, $email_subject, $email_body, $headers);
